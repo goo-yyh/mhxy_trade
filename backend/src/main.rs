cef4f9eb2d3f9a781921b036eefa78c32cf344fe
俞樾珲 <m18758428901@163.com>
2025-12-24 22:45:57 +0800

    feat: init

---
+++ backend/src/main.rs
@@ -0,0 +1,13 @@
+#[tokio::main]
+async fn main() {
+    println!("Hello, world!");
+}
+
+#[cfg(test)]
+mod tests {
+
+    #[test]
+    fn test_main() {
+        assert_eq!(1, 1);
+    }
+}
---
+++ frontend/packages/eslint-config/next.js
@@ -0,0 +1,18 @@
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+
+export default tseslint.config(
+  { ignores: [".next"] },
+  {
+    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    files: ["**/*.{ts,tsx}"],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+  }
+);
