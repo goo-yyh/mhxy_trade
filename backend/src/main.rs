8c01ba4e26ec069dc1460c949ea5cbb22c9e429d
俞樾珲 <m18758428901@163.com>
2025-12-24 23:15:27 +0800

    feat: 修复 github action

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,6 +1,17 @@
+use tracing::{info, level_filters::LevelFilter};
+use tracing_subscriber::{fmt::Layer, layer::SubscriberExt, util::SubscriberInitExt, Layer as _};
+
 #[tokio::main]
 async fn main() {
-    println!("Hello, world!");
+    let layer = Layer::new()
+        .with_target(true) // 显示日志属于哪个模块/文件名
+        .with_line_number(true) // 显示源代码行号
+        .with_thread_ids(true) // 异步环境下查看线程 ID 非常有用
+        .pretty() // 更加易读的多行格式（适合开发环境）
+        .with_filter(LevelFilter::INFO); // 设置日志级别为 INFO
+    tracing_subscriber::registry().with(layer).init();
+
+    info!("Hello, world!");
 }
 
 #[cfg(test)]
